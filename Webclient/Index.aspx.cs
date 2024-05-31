45948bcc08f66bb8feb236f7b4c31fb146baf07b
ameyatgiift <amey.sawant@giift.com>
2024-05-31 13:17:55 +0530

    added pagination css for homepage banner

--- Webclient/Index.aspx.cs
+++ Webclient/Index.aspx.cs
@@ -67,7 +67,7 @@ public partial class Index : Page
 
                     }
                     sb.Append("</div>");
-                    sb.Append("<div class=\"swiper-pagination\"></div>");
+                    sb.Append("<div class=\"swiper-pagination container-lg p-0 text-lg-right\"></div>");
                     sb.Append("<div class=\"swiper-button-prev\">");
                     sb.Append("<img src=\"images/icons/arrows/left-yellow-arrow-2.svg\" alt=\"\" />");
                     sb.Append("</div>");
